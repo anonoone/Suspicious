f1bc9ea60609dbef5259b6613d7966705ade3156
anonoone <128688633+anonoone@users.noreply.github.com>
2023-06-21 09:14:51 +0200

    Add files via upload

--- Suspicious/runall.py
+++ Suspicious/runall.py
@@ -1,35 +1,29 @@
 import os
 
 model=["SGCN","SSGC","SSage"]
-error=[0,0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0]
-data=["Cora","PubMed","Computers","Photo"]
-ano=["Out","MinOnly"]
-for a in ano:
-		for m in model:
-				for d in data:
-						for e in error:
-								os.system("python cleanrun.py --dataset='"+d+"' --vsanomalyrate="+str(e)+" --training='suspicious' --model='"+m+"' --anomaly='"+a+"'") 
-								model=["SGCN","SSGC","SSage"]
-error=[0,0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0]
-data=["Books","reddit"]
-ano=["MinOnly"]
-for a in ano:
-		for m in model:
-				for d in data:
-						for e in error:
-								os.system("python cleanrun.py --dataset='"+d+"' --vsanomalyrate="+str(e)+" --training='suspicious' --model='"+m+"' --anomaly='"+a+"'") 
+model=["SGCN"]
 
-model=["DOMINANT","ANOMALOUS","AnomalyDAE"]
-data=["Cora","PubMed","Computers","Photo"]
-ano=["Out","MinOnly"]
-for a in ano:
-		for m in model:
-				for d in data:
-						os.system("python cleanrun.py --dataset='"+d+"' --training='suspicious' --model='"+m+"' --anomaly='"+a+"'")
-model=["DOMINANT","ANOMALOUS","AnomalyDAE"]
-data=["Books","reddit"]
+#model=["CoLA"]
+#error=[0,0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0]
+#error=[1.0]
+#data=["Cora"]
+#data=["reddit"]	
+#ano=["Con","Str","Min","All","Out","MinOnly"]
+#data=["Cora","PubMed","Computers","Photo","Books","reddit"]
+data=["Computers","Photo","Books"]
+
+anor=[0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09]
 ano=["MinOnly"]
 for a in ano:
 		for m in model:
 				for d in data:
-						os.system("python cleanrun.py --dataset='"+d+"' --training='suspicious' --model='"+m+"' --anomaly='"+a+"'")
+						for ar in anor:
+								os.system("python cleanrun.py --dataset='"+d+"' --training='suspicious' --model='"+m+"' --anomaly='"+a+"' --anomalyrate='"+str(ar)+"'") 
+data=["Cora","Computers","Photo"]
+
+ano=["Out"]
+for a in ano:
+		for m in model:
+				for d in data:
+						for ar in anor:
+								os.system("python cleanrun.py --dataset='"+d+"' --training='suspicious' --model='"+m+"' --anomaly='"+a+"' --anomalyrate='"+str(ar)+"'")
